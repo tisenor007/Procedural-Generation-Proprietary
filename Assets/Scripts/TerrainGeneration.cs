f22a77ca45151222b1cfe05d518621e5367172db
Trenton Isenor <ronesinotnert@gmail.com>
2021-11-09 14:58:42 -0400

    added adjustable sliders to adjust values and generate various map types

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -12,9 +12,13 @@ public class TerrainGeneration : MonoBehaviour
     public float freq;
     [Header("Between 2 and 30, for better results")]
     public float amp;
-    public int chunkSpacing;
+    public int blockSpacing;
     public GameObject prefab;
-    //public Text 
+    public Text mapXTxt;
+    public Text mapZTxt;
+    public Text freqTxt;
+    public Text ampTxt;
+    public Text blockSpacingTxt;
     private List<GameObject> cubes = new List<GameObject>();
     private GameObject map;
     // Start is called before the first frame update
@@ -26,6 +30,11 @@ public class TerrainGeneration : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mapXTxt.text = "New Map X: " + mapSizeX;
+        mapZTxt.text = "New Map Z: " + mapSizeZ;
+        freqTxt.text = "New Frequency: " + freq;
+        ampTxt.text = "New Amp: " + amp;
+        blockSpacingTxt.text = "New Block Spacing: " + blockSpacing;
     }
    
     public void GenerateTerrain()
@@ -39,7 +48,7 @@ public class TerrainGeneration : MonoBehaviour
             for (int x = 0; x < mapSizeX; x++)
             {
                 float y = Mathf.PerlinNoise(x * freq, z * freq) * amp;
-                cubes.Add(Instantiate(prefab, new Vector3(x * chunkSpacing, y, z * chunkSpacing), Quaternion.identity));//GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cubes.Add(Instantiate(prefab, new Vector3(x * blockSpacing, y, z * blockSpacing), Quaternion.identity));//GameObject.CreatePrimitive(PrimitiveType.Cube);
                 //foreach (GameObject cube in cubes) { cube.transform.GetChild(0).gameObject.SetActive(false); }
             }
         }
@@ -47,10 +56,26 @@ public class TerrainGeneration : MonoBehaviour
         foreach (GameObject cube in cubes) { if (cube != null) { cube.transform.parent = map.transform; } }
     }
 
-    public void AdjustMapSizeX(int value)
+    public void AdjustMapSizeX(float value)
     {
-        mapSizeX = value;
+        mapSizeX = (int)value;
     }
-  
-   
+    public void AdjustMapSizeZ(float value)
+    {
+        mapSizeZ = (int)value;
+    }
+    public void AdjustFreq(float value)
+    {
+        freq = value;
+    }
+    public void AdjustAmp(float value)
+    {
+        amp = value;
+    }
+    public void AdjustBlockSpacing(float value)
+    {
+        blockSpacing = (int)value;
+    }
+
+
 }
